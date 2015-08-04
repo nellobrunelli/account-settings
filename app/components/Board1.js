e0c544f8aa07a2c7e01a930a64d4ba8e41205882
Nello Brunelli <nello.brunelli@wyscout.com>
2015-08-04 16:37:39 +0200

    first drag and drop working

--- app/components/Board1.js
+++ app/components/Board1.js
@@ -1,24 +1,77 @@
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend, { NativeTypes } from 'react-dnd/modules/backends/HTML5';
+import Dustbin from './Dustbin';
+import Box from './Box';
+import ItemTypes from './ItemTypes';
+import update from 'react/lib/update';
+
+@DragDropContext(HTML5Backend)
 export default class Board1 extends React.Component {
 
-    // componentWillReceiveProps(nextProps) {
-    //     console.log(nextProps);
-    // }
+  constructor(props) {
+      super(props);
+      this.state = {
+      dustbins: [
+        { accepts: [ItemTypes.GLASS], lastDroppedItem: null },
+        { accepts: [ItemTypes.FOOD], lastDroppedItem: null },
+        { accepts: [ItemTypes.PAPER, ItemTypes.GLASS, NativeTypes.URL], lastDroppedItem: null },
+        { accepts: [ItemTypes.PAPER, NativeTypes.FILE], lastDroppedItem: null }
+      ],
+      boxes: [
+        { name: 'Bottle', type: ItemTypes.GLASS },
+        { name: 'Banana', type: ItemTypes.FOOD },
+        { name: 'Magazine', type: ItemTypes.PAPER }
+      ],
+      droppedBoxNames: []
+    };
+  }
 
-    // ciao mamma sono sul branch
-    render() {
-        return (
-            <div>
-            {this.props.persons.map((el, id) => {
-                return <span key={id}>{el.name}<br /></span>;
-            })}
+  isDropped(boxName) {
+      return this.state.droppedBoxNames.indexOf(boxName) > -1;
+  }
 
-            {this.props.roles.map((el, id) => {
-                return <span key={id}>{el}<br /></span>;
-            })}
-            </div>
+  handleDrop(index, item) {
+      const { name } = item;
 
-        );
-    }
+      this.setState(update(this.state, {
+        dustbins: {
+          [index]: {
+            lastDroppedItem: {
+              $set: item
+            }
+          }
+        },
+        droppedBoxNames: name ? {
+          $push: [name]
+        } : {}
+      }));
+  }
+
+  render() {
+      const { boxes, dustbins } = this.state;
+
+      return (
+      <div>
+        <div style={{ overflow: 'hidden', clear: 'both' }}>
+          {dustbins.map(({ accepts, lastDroppedItem }, index) =>
+            <Dustbin accepts={accepts}
+                     lastDroppedItem={lastDroppedItem}
+                     onDrop={(item) => this.handleDrop(index, item)}
+                     key={index} />
+          )}
+        </div>
+
+        <div style={{ overflow: 'hidden', clear: 'both' }}>
+          {boxes.map(({ name, type }, index) =>
+            <Box name={name}
+                 type={type}
+                 isDropped={this.isDropped(name)}
+                 key={index} />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 Board1.propTypes = {
